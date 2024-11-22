72e40a88835b35f4a3212b95f38322f197a3a819
Andreas Braun <git@alcaeus.org>
2024-11-22 19:05:06 +0100

    Remove unnecessary index

--- src/Document/Partial/PartialStation.php
+++ src/Document/Partial/PartialStation.php
@@ -7,14 +7,12 @@ namespace App\Document\Partial;
 use App\Document\Station;
 use Doctrine\ODM\MongoDB\Mapping\Annotations\EmbeddedDocument;
 use Doctrine\ODM\MongoDB\Mapping\Annotations\EmbedOne;
-use Doctrine\ODM\MongoDB\Mapping\Annotations\Index;
 use Doctrine\ODM\MongoDB\Mapping\Annotations\ReferenceOne;
 
 #[EmbeddedDocument]
 class PartialStation extends AbstractStation
 {
     #[ReferenceOne(name: '_id', storeAs: 'id', targetDocument: Station::class)]
-    #[Index]
     private Station $referencedStation;
 
     #[EmbedOne(targetDocument: PartialAddress::class)]
