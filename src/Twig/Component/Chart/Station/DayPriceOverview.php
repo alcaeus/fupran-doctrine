cb31e3b055cfb4cdcb2e5b43cfddfda41507a843
Andreas Braun <git@alcaeus.org>
2025-07-16 14:25:03 +0200

    Use chart.js to show price overview for current day

--- src/Twig/Component/Chart/Station/DayPriceOverview.php
+++ src/Twig/Component/Chart/Station/DayPriceOverview.php
@@ -4,25 +4,85 @@ declare(strict_types=1);
 
 namespace App\Twig\Component\Chart\Station;
 
-use App\Twig\Component\Chart;
-use DateTimeImmutable;
+use App\Document\DailyPriceReport;
+use App\Document\EmbeddedDailyPrice;
+use App\Document\Price;
+use App\Fuel;
+use DateTimeInterface;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
 #[AsTwigComponent('Chart:Station:DayPriceOverview')]
-class DayPriceOverview extends Chart
+class DayPriceOverview
 {
-    public DateTimeImmutable $date;
-    public bool $allowRangeFilter = false;
+    public DailyPriceReport $priceReport;
 
-    public function getChartId(): string
+    public function __construct(public readonly ChartBuilderInterface $chartBuilder)
     {
-        return $this->chartHelper->stationDayPriceOverview;
     }
 
-    public function getTimestamp(): int
+    public function getChart(): Chart
     {
-        return $this->date
-            ->setTime(0, 0)
-            ->getTimestamp();
+        $chart = $this->chartBuilder->createChart(Chart::TYPE_LINE);
+
+        $chart->setData([
+            'datasets' => $this->createDatasets(),
+        ]);
+
+        $chart->setOptions([
+            'parsing' => false,
+            'datasets' => [
+                'line' => [
+                    'stepped' => 'before',
+                ]
+            ],
+            'scales' => [
+                'x' => [
+                    'type' => 'time',
+                ]
+            ]
+        ]);
+
+        return $chart;
+    }
+
+    private function createDatasets(): array
+    {
+        $datasets = [];
+
+        foreach (Fuel::cases() as $fuel) {
+            $fuelType = $fuel->value;
+            if (! isset($this->priceReport->$fuelType)) {
+                continue;
+            }
+
+            $datasets[] = $this->createDataset($this->priceReport->$fuelType, $fuel);
+        }
+
+        return $datasets;
+    }
+
+    private function createDataset(EmbeddedDailyPrice $dailyPrice, Fuel $fuel): array
+    {
+        return [
+            'label' => $fuel->value,
+            'data' => array_map(
+                fn (Price $price) => [
+                    'x' => $price->date->getTimestamp() * 1000,
+                    'y' => $price->price,
+                ],
+                $dailyPrice->prices->toArray(),
+            )
+        ];
+    }
+
+    private function getTimeInSeconds(DateTimeInterface $dateTime): int
+    {
+        $hours = (int) $dateTime->format('H');
+        $minutes = (int) $dateTime->format('i');
+        $seconds = (int) $dateTime->format('s');
+
+        return $hours * 3600 + $minutes * 60 + $seconds;
     }
 }
