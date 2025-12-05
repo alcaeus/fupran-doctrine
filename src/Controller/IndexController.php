ecd78286cd840d876a8d58ae8238d4b9c63a43b2
Andreas Braun <git@alcaeus.org>
2025-12-05 08:28:43 +0100

    Use attributes, not annotations

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -7,7 +7,7 @@ namespace App\Controller;
 use App\Repository\DailyAggregateRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class IndexController extends AbstractController
 {
--- src/Controller/StationsController.php
+++ src/Controller/StationsController.php
@@ -13,7 +13,7 @@ use Doctrine\ODM\MongoDB\Query\Builder;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class StationsController extends AbstractController
 {
